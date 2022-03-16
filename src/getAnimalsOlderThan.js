c7fe27c661527598987832034d4284ce77046d47
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-16 17:00:37 -0300

    adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
+
+//  testa se a idade de cada animal é igual ou maior que a minima.
+const ageTest = (minAge, residents) => residents.every((ages) => ages.age >= minAge);
+
+//  testa se todos os argumentos sao validos
+const getAnimalsOlderThan = (animal, age) => species.some((animalObj) => animalObj.name === animal
+&& ageTest(age, animalObj.residents));
+
+// console.log(getAnimalsOlderThan('lions', 7));
 
 module.exports = getAnimalsOlderThan;
