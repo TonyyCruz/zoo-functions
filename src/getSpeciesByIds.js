e5271d209addf9db538db99de97a92aed1a407b3
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-16 16:24:16 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+//  abilita species para uso!
+const { species } = data;
+
+//  compara se o id passado é igual ao id dos objetos do array <===
+const comparing = (animalIdComp, id) => id.some((idComp) => animalIdComp.id === idComp);
+
+//  recebe um id e retorna os animais correspondentes <===
+const getSpeciesByIds = (...ids) => species.filter((animalId) => comparing(animalId, ids));
+
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
+// console.log(species);
 
 module.exports = getSpeciesByIds;
