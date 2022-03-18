337403499eae0a2fa9f3434a2d4e39341fefb157
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-18 19:23:51 -0300

    finalmente requisito 7 completo

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -3,7 +3,8 @@ const data = require('../data/zoo_data');
 const { species } = data;
 
 //
-//  sem parametros ou sem a chave includeNames. < ====
+//  Retorno padrão caso não tenha parametros, ou não tenha a chave 'includeNames'. < ====
+//  imprime local e nome. Ex:'NE: [ 'lions', 'giraffes' ]'
 const locationSub = (tip) => species.filter((neLocate) => neLocate.location === tip)
   .map((nam) => nam.name);
 
@@ -30,35 +31,56 @@ const sorted = (use) => {
 
 //  ---------------------------->
 
-//  recebe um animal e retorna seus residentes <====
+// Recebe um nome e sexo, e faz um array com os animais do grupo e sexo especificado <====
+const checkSex = (grup, sx) => {
+  // const grup = Object.keys(animal)[0];
+  const comp = species.find((n) => n.name === grup);
+  const { residents } = comp;
+  return residents.filter((anm) => anm.sex === sx).map((a) => a.name);
+};
+// console.log(checkSex({ lions: ['c', 'b', 'a'] }, 'male'));
+
+//  ---------------------------->
+
+//  recebe um animal e retorna seus residentes, auxiliar de 'includeTrue/ IncludeConstructor'<====
 const reside = (anim) => {
   const select = species.filter((anmFind) => anmFind.name === anim);
   return select[0].residents.map((animals) => animals.name);
 };
 
-//  constroi a estrutura pegida na questão. <====
-const IncludeConstructor = (n) => {
+//  Auxiliar da função 'includeTrue'. <====
+const IncludeConstructor = (n, call, sx) => {
   const nm = [n];
   const back = nm.reduce((acc, cv) => {
-    acc[cv] = reside(cv);
+    acc[cv] = call(cv, sx);
     return acc;
   }, {});
   return back;
 };
 
-//  coloca no formato: { NE: [{animal: [população]}]} <====
-const includeTrue = (locate) => {
+//  Em caso de receber 'includeNames = true', coloca no formato: { NE: [{animal: [população]}]} <====
+// Recebe a saida padrão do 'sem parametro', como parâmetro. - Ex: NE: ['lions', 'giraffes'].
+const includeTrue = (locate, call, sx) => { // o call define quem serão os residentes, deve chamar a função correta
   const { NE, NW, SE, SW } = locate;
-  const ne = NE.map((incube) => IncludeConstructor(incube));
-  const nw = NW.map((incube) => IncludeConstructor(incube));
-  const se = SE.map((incube) => IncludeConstructor(incube));
-  const sw = SW.map((incube) => IncludeConstructor(incube));
+  const ne = NE.map((incube) => IncludeConstructor(incube, call, sx));
+  const nw = NW.map((incube) => IncludeConstructor(incube, call, sx));
+  const se = SE.map((incube) => IncludeConstructor(incube, call, sx));
+  const sw = SW.map((incube) => IncludeConstructor(incube, call, sx));
   return { NE: ne, NW: nw, SE: se, SW: sw };
 };
 
+//  ---------------------------->
+
+const sexSelect = (obj, sexTyp) => includeTrue(obj, checkSex, sexTyp);
+// console.log(sexSelect(location(), 'male').NE);
+
+//  ---------------------------->
+
 // função de controle <====
 const functionsCall = (check) => {
-  const act = includeTrue(location());
+  let act;
+  if (!check.sex) { act = includeTrue(location(), reside); }
+  if (!check.sex === false) { act = sexSelect(location(), check.sex); }
   if (check.sorted === true) { sorted(act); }
   return act;
 };
@@ -69,7 +91,7 @@ const getAnimalMap = (options) => {
   const calls = functionsCall(options);
   return calls;
 };
-console.log(getAnimalMap({ includeNames: true }));
+// console.log(getAnimalMap({ includeNames: true, sex: 'female' }));
 
 //
 
