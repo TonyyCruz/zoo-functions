c8a0f1ce36fa444a8ec1ea869f1739095c67ca7c
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-03-18 22:39:06 -0300

    adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { species, employees } = data;
 
+//  ---------------------------------->
+
+function getOldestFromFirstSpecies(id) {
+  const firstAnimal = employees.find((e) => e.id === id).responsibleFor[0];
+  const animals = species.find((a) => a.id === firstAnimal).residents;
+  const result = animals.reduce((acc, cv) => {
+    if (acc.age > cv.age) { return acc; }
+    return cv;
+  });
+  return [result.name, result.sex, result.age];
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
